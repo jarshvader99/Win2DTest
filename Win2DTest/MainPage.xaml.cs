cf85ae2464c33ce9d3c10baed61e96433f18f898
jarshvader99 <jarshvader@gmail.com>
2021-01-18 09:03:54 -0600

    adding dynamically generated drawing parameters

--- Win2DTest/MainPage.xaml.cs
+++ Win2DTest/MainPage.xaml.cs
@@ -26,6 +26,23 @@ namespace Win2DTest
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        Random rnd = new Random();
+        private Vector2 RndPosition()
+        {
+            double x = rnd.NextDouble() * 500f;
+            double y = rnd.NextDouble() * 500f;
+            return new Vector2((float)x, (float)y);
+        }
+
+        private float RndRadius()
+        {
+            return (float)rnd.NextDouble() * 150f;
+        }
+
+        private byte RndByte()
+        {
+            return (byte)rnd.Next(256);
+        }
         public MainPage()
         {
             this.InitializeComponent();
@@ -34,7 +51,23 @@ namespace Win2DTest
 
         private void canvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
         {
+            /*
             args.DrawingSession.DrawText("Hello, World!", 100, 100, Colors.White);
+            args.DrawingSession.DrawCircle(125, 125, 100, Colors.Green);
+            args.DrawingSession.DrawLine(0, 0, 50, 200, Colors.Red);
+            */
+            for (int i = 0; i < 25; i++)
+            {
+                args.DrawingSession.DrawText("Hello, World!", RndPosition(), Color.FromArgb(255, RndByte(), RndByte(), RndByte()));
+                args.DrawingSession.DrawCircle(RndPosition(), RndRadius(), Color.FromArgb(255, RndByte(), RndByte(), RndByte()));
+                args.DrawingSession.DrawLine(RndPosition(), RndPosition(), Color.FromArgb(255, RndByte(), RndByte(), RndByte()));
+            }
+        }
+
+        void Page_Unloaded(object sender, RoutedEventArgs e)
+        {
+            this.canvas.RemoveFromVisualTree();
+            this.canvas = null;
         }
     }
 }
